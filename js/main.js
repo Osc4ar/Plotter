91a9d852738b54134d7b6d57ad89ca7015354a34
Oscar I. Castillo <oscar_97.10@outlook.com>
2018-05-09 00:15:19 -0500

    UI implemented and basic chart functions

---
+++ js/main.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+    $('select').formSelect();
+});
+
+var slider = document.getElementById('test-slider');
+  noUiSlider.create(slider, {
+   start: [-10, 10],
+   connect: true,
+   step: 1,
+   orientation: 'horizontal', // 'horizontal' or 'vertical'
+   range: {
+     'min': -10,
+     'max': 10
+   }
+  });
+
+window.onload = function () {
+    var expr = "sin(x)";
+    var data_var = [];
+    for (let x = -5; x <= 5; x += 0.001) {
+        data_var.push({y: Math.sin(x)});
+    }
+    var chart = new CanvasJS.Chart("chartContainer", {
+        animationEnabled: true,
+        
+        theme: "light2",
+        title: {
+            text: expr
+        },
+        axisX: {
+            title: "x"
+        },
+        axisY: {
+            title: expr
+        },
+        data: [{
+            type: "line",
+            color: "#00C2AD",
+            dataPoints: data_var
+        }]
+    });
+    chart.render();
+}
