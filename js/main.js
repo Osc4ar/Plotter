ae86b11119403e518e342ba7adcc9af9f4517f93
Oscar I. Castillo <oscar_97.10@outlook.com>
2018-05-09 14:46:12 -0500

    Working enough

--- js/main.js
+++ js/main.js
@@ -4,40 +4,42 @@ $(document).ready(function() {
 
 var slider = document.getElementById('test-slider');
   noUiSlider.create(slider, {
-   start: [-10, 10],
+   start: [-5, 5],
    connect: true,
    step: 1,
    orientation: 'horizontal', // 'horizontal' or 'vertical'
    range: {
-     'min': -10,
-     'max': 10
+     'min': -50,
+     'max': 50
    }
   });
 
 window.onload = plot();
-  
+
 function plot() {
     var func_sel = document.getElementById("func_sel");
-    var func = func_sel.options[func_sel.selectedIndex].text;
-    var arg = document.getElementById("argument");
-    var fullExpr = func + "(" + arg + ")";
-    console.log(fullExpr);
+    var func = func_sel.options[func_sel.selectedIndex].value;
+    var arg = document.getElementById("argument").value;
+    if(arg == "")
+        arg = "x";
+    var fullExpr = "Math." + func + "(" + arg + ")";
+    if (func == "f")
+        fullExpr = arg;
     var data_var = [];
-    for (let x = -5; x <= 5; x += 0.001) {
-        data_var.push({y: Math.sin(x)});
+    var range = slider.noUiSlider.get();
+    for (let index = 0; index < range.length; index++)
+        range[index] = parseInt(range[index]);
+    for (let x = range[0]; x <= range[1]; x += 0.001) {
+        data_var.push({x: x, y: eval(fullExpr)});
     }
     var chart = new CanvasJS.Chart("chartContainer", {
         animationEnabled: true,
-        
         theme: "light2",
-        title: {
-            text: expr
-        },
         axisX: {
             title: "x"
         },
         axisY: {
-            title: expr
+            title: "f(x)"
         },
         data: [{
             type: "line",
